85fe038d0b58fd303e834a42d45275fa55caa558
vasovrk <vasovrk@gmail.com>
2015-01-12 19:30:09 +0200

    project creation

---
+++ Assets/Scripts/Generate.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Generate : MonoBehaviour {
+	public GameObject rocks;
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating ("CreateObstacle", 4f, 2f);
+	}
+	
+	// Update is called once per frame
+	void CreateObstacle(){
+		Instantiate (rocks);
+		//DestroyImmediate (rocks,true);
+		//TODO: destroy rocks after getting
+	}
+//	void update(){
+//		Vector2 screenPosition = Camera.main.WorldToScreenPoint(transform.position);
+//		if (screenPosition.x > Screen.height || screenPosition.y < 0)
+//		{
+//			Destroy(this.rocks);
+//		}
+//		}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	public Vector2 jumpForce = new Vector2(0, 300);
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyUp("space")){
+			rigidbody2D.velocity = Vector2.zero;
+			rigidbody2D.AddForce(jumpForce);
+		}
+		Vector2 screenPosition = Camera.main.WorldToScreenPoint(transform.position);
+		if (screenPosition.y > Screen.height || screenPosition.y < 0)
+		{
+			Die();
+		}
+}
+	void OnCollisionEnter2D(Collision2D other)//whenever there is a collision reset level
+	{
+		Die();
+	}
+	
+	void Die()//level reset
+	{
+		Application.LoadLevel(Application.loadedLevel);
+	}
+}
